68ce7e69e813c32ec07fc225d837a0e7b468451a
Freddie Mason <fmason@redev.co.uk>
2024-09-20 12:35:53 +0100

    started making db

---
+++ Electrify/Electrify.DlmsServer/Database/ElectrifyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Electrify.Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Electrify.DlmsServer.Database;
+
+/// <summary>
+/// DB context for Electrify.
+/// </summary>
+public class ElectrifyDbContext(DbContextOptions<ElectrifyDbContext> options) : DbContext(options)
+{
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Reading> Readings { get; set; }
+
+}
---
+++ Electrify/Electrify.Models/Models/Reading.cs
@@ -0,0 +1,12 @@
+﻿namespace Electrify.Models.Models;
+
+public class Reading
+{
+    public Reading() 
+    {
+    }
+
+    public required int Id { get; set; }
+    public required int ClientId { get; set; }
+    public required string MeterReading { get; set; }
+}
