2ccf89bf1c42d43e538bc432740208ab81dca535
Freddie Mason <115559911+fM4s0n@users.noreply.github.com>
2024-09-20 10:45:19 +0100

    Chore/add sln UI proj (#3)

---
+++ Electrify/Electrify.AdminUi/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Electrify.AdminUi;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Electrify/Electrify.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Electrify.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
