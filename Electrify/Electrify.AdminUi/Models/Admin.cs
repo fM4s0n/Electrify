121e3c119a11b302fac15f68e48a508e815f4777
Freddie Mason <fmason@redev.co.uk>
2024-09-20 11:14:17 +0100

    added admin

---
+++ Electrify/Electrify.AdminUi/Models/Admin.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Electrify.AdminUi.Models;
+
+/// <summary>
+/// Model for the Electrify Admin.
+/// </summary>
+public class Admin
+{
+    private static readonly PasswordHasher<Admin> hasher = new();
+
+    public Admin()
+    {
+    }
+
+    public Admin(string name, string email, string plainTextPassword)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Email = email;        
+        PasswordHash = hasher.HashPassword(this, plainTextPassword);
+    }
+
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+}
