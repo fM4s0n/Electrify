cce8846bf5d0b8315744d12468d368a0cff99323
Freddie Mason <fmason@redev.co.uk>
2024-09-20 12:39:54 +0100

    updated to record

--- Electrify/Electrify.Models/Models/Reading.cs
+++ Electrify/Electrify.Models/Models/Reading.cs
@@ -1,11 +1,7 @@
 ﻿namespace Electrify.Models.Models;
 
-public class Reading
+public sealed record Reading
 {
-    public Reading() 
-    {
-    }
-
     public required int Id { get; set; }
     public required int ClientId { get; set; }
     public required string MeterReading { get; set; }
