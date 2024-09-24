fee0f6db547720011b674e2f00df78be6f614e31
Freddie Mason <fmason@redev.co.uk>
2024-09-24 16:47:33 +0100

    added verify password method to admin

--- Electrify/Electrify.Models/Models/Admin.cs
+++ Electrify/Electrify.Models/Models/Admin.cs
@@ -10,9 +10,7 @@ public class Admin
     // TODO: MAKE THIS A FACTORY
     private static readonly PasswordHasher<Admin> hasher = new();
 
-    public Admin()
-    {
-    }
+    public Admin() { }
 
     public Admin(string name, string email, string plainTextPassword)
     {
@@ -26,4 +24,14 @@ public class Admin
     public required string Name { get; set; }
     public required string Email { get; set; }
     public required string PasswordHash { get; set; }
+
+    /// <summary>
+    /// Valuidates the password passed in from the user.
+    /// </summary>
+    /// <param name="plainTextPassword">The password provided by the user.</param>
+    /// <returns>True if the result is Success or SuccessRehashNeeded, otherwise false.</returns>
+    public bool VerifyPassword(string plainTextPassword)
+    {
+        return hasher.VerifyHashedPassword(this, PasswordHash, plainTextPassword) != PasswordVerificationResult.Failed;
+    }
 }
