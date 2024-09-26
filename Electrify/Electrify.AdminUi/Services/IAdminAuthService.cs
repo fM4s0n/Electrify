b1c0f06f5d71c34db4d8116ea70a5346f8b05b75
Freddie Mason <fmason@redev.co.uk>
2024-09-26 16:25:00 +0100

    added admin auth service to admin ui project

---
+++ Electrify/Electrify.AdminUi/Services/IAdminAuthService.cs
@@ -0,0 +1,17 @@
+﻿using Electrify.Models.Models;
+
+namespace Electrify.AdminUi.Services;
+
+/// <summary>
+/// Auth service for admins.
+/// </summary>
+internal interface IAdminAuthService
+{
+    /// <summary>
+    /// Verifies the password of the admin account is correct.
+    /// </summary>
+    /// <param name="admin">Admin account attempting to login</param>
+    /// <param name="plainTextPassword">The plain password passed in by the user.</param>
+    /// <returns>True if Success or SuccessNeedsRehash, otherwise returns false.</returns>
+    bool VerifyPassword(Admin admin, string plainTextPassword);
+}
---
+++ Electrify/Electrify.AdminUi/Services/Implementations/AdminAuthService.cs
@@ -0,0 +1,13 @@
+﻿using Electrify.Models.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Electrify.AdminUi.Services.Implementations;
+
+internal class AdminAuthService : IAdminAuthService
+{
+    private readonly PasswordHasher<Admin> hasher = new();
+    public bool VerifyPassword(Admin admin, string plainTextPassword)
+    {
+        return hasher.VerifyHashedPassword(admin, admin.PasswordHash, plainTextPassword) != PasswordVerificationResult.Failed;
+    }
+}
