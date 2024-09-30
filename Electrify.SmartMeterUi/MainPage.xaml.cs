eb87dce78a0ce9f1f7be5a6631ed217e664184a4
Josh Cartledge <jcartledge@redev.co.uk>
2024-09-30 16:28:15 +0100

    Fixed broken commit

--- Electrify.SmartMeterUi/MainPage.xaml.cs
+++ Electrify.SmartMeterUi/MainPage.xaml.cs
@@ -5,5 +5,32 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+
+        SetMainWindowSize();
+    }
+
+    /// <summary>
+    /// Sets the default fixed screen size for the application
+    /// </summary>
+    private void SetMainWindowSize()
+    {
+        const float width = 1024f;
+        const float height = 600f;
+
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(
+            nameof(IWindow), (handler, _) =>
+            {
+                if(handler.PlatformView.WindowScene is { SizeRestrictions: not null })
+                    Task.Run(() =>
+                    {
+                        MainThread.BeginInvokeOnMainThread(() =>
+                        {
+                            handler.PlatformView.WindowScene.SizeRestrictions.MinimumSize =
+                                new CoreGraphics.CGSize(width, height);
+                            handler.PlatformView.WindowScene.SizeRestrictions.MaximumSize =
+                                new CoreGraphics.CGSize(width, height);
+                        });
+                    });
+            });
     }
 }
