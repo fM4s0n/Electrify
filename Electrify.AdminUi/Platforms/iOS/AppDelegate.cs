5377c6d89fbb6c6068d18c9f65da579d3a9066b1
Freddie Mason <fmason@redev.co.uk>
2024-11-08 13:11:53 +0000

    re did services to be in speerate project to avoid maui issues

--- Electrify.AdminUi/Platforms/iOS/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace Electrify.AdminUi;
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
