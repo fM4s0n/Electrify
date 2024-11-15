f296613b868ac13548698b796950db1744b3c085
Freddie Mason <115559911+fM4s0n@users.noreply.github.com>
2024-11-15 10:12:28 +0000

    removed on model creating (#41)

--- Electrify.Server/Database/ElectrifyDbContext.cs
+++ Electrify.Server/Database/ElectrifyDbContext.cs
@@ -11,11 +11,4 @@ public class ElectrifyDbContext(DbContextOptions<ElectrifyDbContext> options) : 
     public DbSet<Admin> Admins { get; set; }    
     public DbSet<Client> Clients { get; set; }    
     public DbSet<Reading> Readings { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Admin>().HasKey(a => a.Id); // is this right?
-        modelBuilder.Entity<Client>().HasKey(c => c.UserId);
-        modelBuilder.Entity<Reading>().HasKey(r => r.Id);
-    }
 }
