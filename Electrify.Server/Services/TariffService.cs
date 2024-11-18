e6e00e47739689075f3035dd182fe046af3c0018
James Lloyd <jlloyd@redev.co.uk>
2024-11-18 10:46:05 +0000

    put updatetariff code back

--- Electrify.Server/Services/TariffService.cs
+++ Electrify.Server/Services/TariffService.cs
@@ -31,51 +31,51 @@ public class TariffService(
     
     private async Task UpdateTariff()
     {
-        // using var scope = serviceProvider.CreateScope();
-        // var database = scope.ServiceProvider.GetRequiredService<ElectrifyDbContext>();
-        //
-        // var localNow = timeProvider.GetLocalNow();
-        //
-        // var currentTariff = database.Tariffs.FirstOrDefault(
-        //     t => t.DateTime.Date == localNow.Date && t.DateTime.Hour == localNow.Hour);
-        //
-        // if (currentTariff is null)
-        // {
-        //     var daysPrices = await octopusService.GetDailyPrices(localNow);
-        //
-        //     if (daysPrices is null)
-        //     {
-        //         currentTariff = database.Tariffs.MaxBy(t => t.DateTime);
-        //     }
-        //     else
-        //     {
-        //         foreach (var (hour, price) in daysPrices)
-        //         {
-        //             var dateTime = new DateTime(localNow.Year, localNow.Month, localNow.Day, hour, 0, 0);
-        //             
-        //             await database.Tariffs.AddAsync(new Tariff
-        //             {
-        //                 DateTime = dateTime,
-        //                 Price = price
-        //             });
-        //         }
-        //             
-        //         await database.SaveChangesAsync();
-        //
-        //         currentTariff = database.Tariffs.FirstOrDefault(
-        //             t => t.DateTime.Date == localNow.Date && t.DateTime.Hour == localNow.Hour);
-        //     }
-        // }
-        //
-        // if (currentTariff is null)
-        // {
-        //     logger.LogError("No tariff was found in the database or the octopus API for the current time");
-        //     return;
-        // }
+        using var scope = serviceProvider.CreateScope();
+        var database = scope.ServiceProvider.GetRequiredService<ElectrifyDbContext>();
+        
+        var localNow = timeProvider.GetLocalNow();
+        
+        var currentTariff = database.Tariffs.FirstOrDefault(
+            t => t.DateTime.Date == localNow.Date && t.DateTime.Hour == localNow.Hour);
+        
+        if (currentTariff is null)
+        {
+            var daysPrices = await octopusService.GetDailyPrices(localNow);
+        
+            if (daysPrices is null)
+            {
+                currentTariff = database.Tariffs.MaxBy(t => t.DateTime);
+            }
+            else
+            {
+                foreach (var (hour, price) in daysPrices)
+                {
+                    var dateTime = new DateTime(localNow.Year, localNow.Month, localNow.Day, hour, 0, 0);
+                    
+                    await database.Tariffs.AddAsync(new Tariff
+                    {
+                        DateTime = dateTime,
+                        Price = price
+                    });
+                }
+                    
+                await database.SaveChangesAsync();
+        
+                currentTariff = database.Tariffs.FirstOrDefault(
+                    t => t.DateTime.Date == localNow.Date && t.DateTime.Hour == localNow.Hour);
+            }
+        }
+        
+        if (currentTariff is null)
+        {
+            logger.LogError("No tariff was found in the database or the octopus API for the current time");
+            return;
+        }
     
         foreach (var dlmsClient in dlmsClientService.GetClients())
         {
-            dlmsClient.WriteTariff(Random.Shared.NextDouble());
+            dlmsClient.WriteTariff(currentTariff.Price);
         }
     }
 
