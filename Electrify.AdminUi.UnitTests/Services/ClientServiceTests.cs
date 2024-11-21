6c5754f3e9e3b2952d4e5899cb0dfcb613aae9e1
Freddie Mason <115559911+fM4s0n@users.noreply.github.com>
2024-11-21 09:37:11 +0000

    added client service tests (#49)

---
+++ Electrify.AdminUi.UnitTests/Services/ClientServiceTests.cs
@@ -0,0 +1,61 @@
+﻿
+using Electrify.AdminUi.Services;
+using Electrify.Models;
+using Electrify.Server.ApiClient.Abstraction;
+using Electrify.Server.ApiClient.Contracts;
+using FluentAssertions;
+using NSubstitute;
+
+namespace Electrify.AdminUi.UnitTests.Services;
+
+public class ClientServiceTests
+{
+    private readonly IElectrifyApiClient _electrifyApiClient;
+
+    public ClientServiceTests()
+    {
+        _electrifyApiClient = Substitute.For<IElectrifyApiClient>();
+    }
+
+    [Fact]
+    public async Task InsertClient_Should_Return_True_When_Response_IsTrue()
+    {
+        // Arrange
+        _electrifyApiClient.InsertClient(Arg.Any<Guid>(), Arg.Any<Guid>())
+            .Returns(new HttpInsertClientResponse { Success = true });
+
+        var clientService = new ClientService(_electrifyApiClient);
+        var newClient = new Client
+        {
+            Id = Guid.NewGuid(),
+            UserId = Guid.NewGuid()
+        };
+
+        // Act
+        var result = await clientService.InsertClient(newClient);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task InsertClient_Should_Return_False_When_Response_IsFalse()
+    {
+        // Arrange
+        _electrifyApiClient.InsertClient(Arg.Any<Guid>(), Arg.Any<Guid>())
+            .Returns(new HttpInsertClientResponse { Success = false });
+
+        var clientService = new ClientService(_electrifyApiClient);
+        var newClient = new Client
+        {
+            Id = Guid.NewGuid(),
+            UserId = Guid.NewGuid()
+        };
+
+        // Act
+        var result = await clientService.InsertClient(newClient);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+}
