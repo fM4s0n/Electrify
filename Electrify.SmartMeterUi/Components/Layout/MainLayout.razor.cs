b47cec2f55c6dae2c3200536987dc672e3b61f3c
JoshCartledge3 <106667950+JoshCartledge3@users.noreply.github.com>
2024-11-26 10:51:47 +0000

    Added reconnect and port variable (#66)
    Co-authored-by: Josh Cartledge <jcartledge@redev.co.uk>

--- Electrify.SmartMeterUi/Components/Layout/MainLayout.razor.cs
+++ Electrify.SmartMeterUi/Components/Layout/MainLayout.razor.cs
@@ -1,46 +1,17 @@
-using Electrify.Dlms.Options;
-using Electrify.Dlms.Server.Abstraction;
-using Electrify.Server.ApiClient;
 using Electrify.SmartMeterUi.Services.Abstractions;
 using Microsoft.AspNetCore.Components;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Serilog.Extensions.Logging;
 
 namespace Electrify.SmartMeterUi.Components.Layout;
 
 public partial class MainLayout
 {
-    [Inject] private IHttpClientFactory ClientFactory { get; set; } = default!;
-    [Inject] private IOptions<DlmsServerOptions> Options { get; set; } = default!;
-    [Inject] private IDlmsServer DlmsServer { get; set; } = default!;
-    [Inject] private IErrorMessageService ErrorMessageService { get; set; } = default!;
-    
-    private ElectrifyApiClient _apiClient = default!;
-    
+    [Inject] private IConnectionService ConnectionService { get; set; } = default!;
+
     protected override async Task OnInitializedAsync()
     {
-        ILogger<ElectrifyApiClient> logger = new Logger<ElectrifyApiClient>(new SerilogLoggerFactory());
-        
-        _apiClient = new ElectrifyApiClient(ClientFactory.CreateClient("ElectrifyServer"), logger);
-        await Task.WhenAll(InitDlmsServer(), RegisterConnectionWithServer());
-    }
-    
-    private async Task InitDlmsServer()
-    {
-        Task.Run(() =>
-        {
-            DlmsServer.Initialise(
-                Options,
-                () => ErrorMessageService.IsConnected = true,
-                () => ErrorMessageService.IsConnected = false,
-                () => ErrorMessageService.ErrorMessage = DlmsServer.GetErrorMessage());
-        });
-    }
-
-    private async Task RegisterConnectionWithServer()
-    {
-        await _apiClient.Register(Options.Value.Port, Options.Value.Password, Guid.NewGuid());
+        // Make an initial connection to the server
+        await ConnectionService.InitializeConnectionAsync(false);
     }
 }
+
 
